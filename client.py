0d8998ac17cae55e119ce879fdb2aef989d75a21
Anjali Bothra <ab1865@ilab1.cs.rutgers.edu>
2022-02-21 21:27:50 -0500

    read from text file update

--- client.py
+++ client.py
@@ -9,10 +9,16 @@ localhost_addr = socket.gethostbyname(socket.gethostname())
     # connect to the server on local machine
 server_binding = (localhost_addr, port)
 cs.connect(server_binding)
-msg = "GOOGLE.com"
-cs.send(msg.encode("utf-8"))
-data = cs.recv(200)
-print("Returned data: ", data.decode('utf-8'))
-print( "[C]: Group-{group_idx} Clien-{idx} received from server: {data.decode('utf-8')}")
-cs.close()
+#anjali - need to open the file, proj2-HNS.txt, and load that stuff to the message
+#then you need to print out what its supposed to say on another file
+
+with open('PROJ2-HNS.txt') as textfile:
+    for line in textfile:
+        print(line)
+        msg = line.strip()
+        cs.send(msg.encode("utf-8"))
+        data = cs.recv(200)
+        print("Returned data: ", data.decode('utf-8'))
+        print( "[C]: Group-{group_idx} Clien-{idx} received from server: {data.decode('utf-8')}")
+cs.close()        
 exit()
